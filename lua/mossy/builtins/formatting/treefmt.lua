9b8a8e04c0dea5bb909490ecabc0a0649e711d8b
robin <comfysage@isabelroses.com>
2025-08-30 13:43:11 +0200

    builtins.formatting.treefmt: fix cond fn params

--- lua/mossy/builtins/formatting/treefmt.lua
+++ lua/mossy/builtins/formatting/treefmt.lua
@@ -7,7 +7,7 @@ return {
     local filename = vim.api.nvim_buf_get_name(params.buf)
     return { '--allow-missing-formatter', '--stdin', filename }
   end,
-  cond = function()
+  cond = function(_)
     return vim.fs.root(0, 'treefmt.toml')
   end,
 }
